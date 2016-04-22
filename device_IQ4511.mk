be019f8adb616415b565d0e469d6e5ee2ecb08e3
hyperion70 <bmaximov71@gmail.com>
2016-04-22 09:10:19 +0300

    media_codecs.xml

--- device_IQ4511.mk
+++ device_IQ4511.mk
@@ -132,8 +132,8 @@ PRODUCT_PACKAGES += \
     libtinyxml
 
 PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/media_codecs2.xml:system/etc/media_codecs2.xml \
 	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/permissions/media_codecs.xml \
 	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 PRODUCT_PROPERTY_OVERRIDES := \
